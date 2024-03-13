7f6d58d16ee428084c60bfa4090dc130e5e8c5d7
shubham thorat <shubham230520011@gmail.com>
2024-03-13 16:25:57 +0530

    second commit

---
+++ lib/database/firestore.dart
@@ -0,0 +1,29 @@
+/*
+
+This database stores posts that user have published in the app. It is stored in collections called 'Posts' in Firebase.
+
+*/
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FireStoreDatabase {
+  User? user = FirebaseAuth.instance.currentUser;
+  final CollectionReference posts =
+      FirebaseFirestore.instance.collection('Posts');
+
+  Future<void> addPost(String message) {
+    return posts.add({
+      'UserEmail': user!.email,
+      'PostMessage': message,
+      'TimeStamp': Timestamp.now(),
+    });
+  }
+
+  Stream<QuerySnapshot> getPostsStream() {
+    final postsStream = FirebaseFirestore.instance
+        .collection('Posts')
+        .orderBy('TimeStamp', descending: true)
+        .snapshots();
+    return postsStream;
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:minimilist_social_media_app/components/my_list_tile.dart';
+import 'package:minimilist_social_media_app/components/my_post_button.dart';
 import 'package:minimilist_social_media_app/components/my_text_field.dart';
+import 'package:minimilist_social_media_app/database/firestore.dart';
 
 class HomePage extends StatelessWidget {
   void logout() {
@@ -9,7 +14,18 @@ class HomePage extends StatelessWidget {
 
   HomePage({super.key});
 
+  final FireStoreDatabase database = FireStoreDatabase();
+
   final TextEditingController newPostController = TextEditingController();
+
+  void postMessage() {
+    if (newPostController.text.isNotEmpty) {
+      String message = newPostController.text;
+      database.addPost(message);
+    }
+
+    newPostController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +41,55 @@ class HomePage extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(25.0),
-            child: MyTextField(
-                hintText: "Say something..",
-                obscureText: false,
-                controller: newPostController),
-          )
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyTextField(
+                      hintText: "Say something..",
+                      obscureText: false,
+                      controller: newPostController),
+                ),
+                PostButton(onTap: () {
+                  postMessage;
+                })
+              ],
+            ),
+          ),
+          StreamBuilder(
+              stream: database.getPostsStream(),
+              builder: ((context, snapshot) {
+                // show loading circle
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final posts = snapshot.data!.docs;
+
+                if (snapshot.data == null || posts.isEmpty) {
+                  return const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(25),
+                      child: Text("No Posts.. Post something!"),
+                    ),
+                  );
+                }
+
+                return Expanded(
+                    child: ListView.builder(
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) {
+                    final post = posts[index];
+                    
+                    String message = post['PostMessage'];
+                    String userEmail = post['UserEmail'];
+                    Timestamp timeStamp = post['TimeStamp'];
+
+                    return MyListTile(title: message, subtitle: userEmail);
+                  },
+                ));
+              }))
         ],
       ),
     );
